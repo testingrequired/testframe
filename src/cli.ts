cd40553a279720466bdc690d4989e2abce7a19f5
Kylee Tilley <kyleetilley@gmail.com>
2019-03-23 16:48:30 -0500

    Adding glob functionality

--- src/cli.ts
+++ src/cli.ts
@@ -3,4 +3,4 @@ import globTestFiles from "./middlewear/globTestFiles";
 import runTests from "./middlewear/runTests";
 import printResults from "./middlewear/printResults";
 
-tf(globTestFiles, runTests, printResults);
+tf(globTestFiles("./tests/*.test.js"), runTests, printResults);
--- src/middlewear/globTestFiles.ts
+++ src/middlewear/globTestFiles.ts
@@ -1,9 +1,12 @@
+import glob from "glob";
+
 import Setup from "../Setup";
 import Results from "../Results";
 
-export default async (setup: Setup, results: Results) => {
-  setup.testFiles.set(
-    "../../tests/example.test.js",
-    require("../../tests/example.test.js")
-  );
+export default (pattern: string) => (setup: Setup, results: Results) => {
+  const paths = glob.sync(pattern, { absolute: true });
+
+  paths.forEach(path => {
+    setup.testFiles.set(path, require(path));
+  });
 };

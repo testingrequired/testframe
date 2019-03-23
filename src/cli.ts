a9787c08e5723937941965eafa1d3b1c04947406
Kylee Tilley <kyleetilley@gmail.com>
2019-03-23 16:54:00 -0500

    Refactor

--- src/cli.ts
+++ src/cli.ts
@@ -1,6 +1,6 @@
 import tf from "./index";
 import globTestFiles from "./middlewear/globTestFiles";
-import runTests from "./middlewear/runTests";
-import printResults from "./middlewear/printResults";
+import runFlatTests from "./middlewear/runFlatTests";
+import printResultsToConsole from "./middlewear/printResultsToConsole";
 
-tf(globTestFiles("./tests/*.test.js"), runTests, printResults);
+tf(globTestFiles("./tests/*.test.js"), runFlatTests, printResultsToConsole);
--- src/middlewear/globTestFiles.ts
+++ src/middlewear/globTestFiles.ts
@@ -1,10 +1,11 @@
 import glob from "glob";
 
 import Setup from "../Setup";
-import Results from "../Results";
 
-export default (pattern: string) => (setup: Setup, results: Results) => {
-  const paths = glob.sync(pattern, { absolute: true });
+export default (pattern: string, cwd: string = process.cwd()) => (
+  setup: Setup
+) => {
+  const paths = glob.sync(pattern, { cwd, absolute: true });
 
   paths.forEach(path => {
     setup.testFiles.set(path, require(path));

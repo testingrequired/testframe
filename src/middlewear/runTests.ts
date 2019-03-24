0d3e9a9d98c2b89679abb8a64af857bfb6df5716
Kylee Tilley <kyleetilley@gmail.com>
2019-03-23 23:56:53 -0500

    Add additional hooks

--- src/middlewear/runTests.ts
+++ src/middlewear/runTests.ts
@@ -8,8 +8,17 @@ export default function runTests(setup: Setup, results: Results) {
   testFilePaths.forEach(testFilePath => {
     results[testFilePath] = {};
 
+    const beforeAlls = [];
+    (global as any).beforeAll = fn => beforeAlls.push(fn);
+
     const beforeEachs = [];
     (global as any).beforeEach = fn => beforeEachs.push(fn);
+
+    const afterEachs = [];
+    (global as any).afterEach = fn => afterEachs.push(fn);
+
+    const afterAlls = [];
+    (global as any).afterAll = fn => afterAlls.push(fn);
 
     const tests = [];
     (global as any).test = (description: string, fn: () => void) =>
@@ -17,12 +26,15 @@ export default function runTests(setup: Setup, results: Results) {
 
     require(testFilePath);
 
+    beforeAlls.forEach(beforeAll => beforeAll());
+
     tests.forEach(([description, test]) => {
       let result;
 
       try {
         beforeEachs.forEach(beforeEach => beforeEach());
         test(assert, components);
+        afterEachs.forEach(afterEach => afterEach());
         result = true;
       } catch (e) {
         result = e;
@@ -30,5 +42,7 @@ export default function runTests(setup: Setup, results: Results) {
         results[testFilePath][description] = result;
       }
     });
+
+    afterAlls.forEach(afterAll => afterAll());
   });
 }
--- tests/example.test.js
+++ tests/example.test.js
@@ -1,7 +1,19 @@
 let value;
 
+beforeAll(() => {
+  console.log("Before All");
+});
+
 beforeEach(() => {
   value = 10;
+});
+
+afterEach(() => {
+  console.log("After Each");
+});
+
+afterAll(() => {
+  console.log("After All");
 });
 
 test(`test1`, assert => assert(10 === value, "The error message"));

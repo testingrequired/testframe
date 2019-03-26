1dc1ec94368e857ccb1ba5dac0f25078e4c64139
Kylee Tilley <kyleetilley@gmail.com>
2019-03-25 20:40:20 -0500

    Add error checking for cli file path

--- src/cli.ts
+++ src/cli.ts
@@ -1,11 +1,15 @@
-require = require("esm")(module /*, options*/);
-
-const path = require("path");
+import path from "path";
 
 export const run = () => {
   const cliFile = process.argv.slice(2)[0];
 
-  const run = require(path.join(process.cwd(), cliFile)).default;
+  if (!cliFile) {
+    throw new Error("Must define cli file to use");
+  }
+
+  const { default: run } = require("esm")(module)(
+    path.join(process.cwd(), cliFile)
+  );
 
   run();
 };

bf17049883bf2fa9d2cc56029400cdf1fd8046fe
Kylee Tilley <kyleetilley@gmail.com>
2019-03-25 21:44:40 -0500

    Add callback test

---
+++ src/middlewear/callback.test.ts
@@ -0,0 +1,24 @@
+import callback from "./callback";
+import Setup from "../types/Setup";
+import Results from "../types/Results";
+
+describe("callback", () => {
+  let fn;
+
+  const setup: Setup = {
+    testFilePaths: [],
+    components: {},
+    tests: []
+  };
+
+  const results: Results = {};
+
+  beforeEach(() => {
+    fn = jest.fn();
+  });
+
+  it("should call fn with setup and results", () => {
+    callback(fn)(setup, results);
+    expect(fn).toBeCalledWith(setup, results);
+  });
+});

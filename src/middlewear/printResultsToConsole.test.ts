aa1a46a33095b1260bd2bddb03264b036253ce41
Kylee Tilley <kyleetilley@gmail.com>
2019-04-02 10:56:03 -0500

    Add printResultsToConsole test

---
+++ src/middlewear/printResultsToConsole.test.ts
@@ -0,0 +1,32 @@
+import printResultsToConsole from "./printResultsToConsole";
+import Setup from "../types/Setup";
+import Results from "../types/Results";
+
+describe("printResultsToConsole", () => {
+  const setup: Setup = {
+    testFilePaths: [],
+    components: {},
+    tests: []
+  };
+
+  const results: Results = [
+    {
+      description: ["foo", "bar"],
+      state: "passed",
+      start: new Date(),
+      end: new Date()
+    }
+  ];
+
+  let logSpy;
+
+  beforeEach(() => {
+    logSpy = jest.spyOn(console, "log");
+  });
+
+  it("should call console.log with json stringified results", () => {
+    printResultsToConsole(setup, results);
+
+    expect(logSpy).toBeCalledWith(JSON.stringify(results, null, 2));
+  });
+});

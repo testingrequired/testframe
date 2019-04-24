a77c71fce7dc7dbcd3c6824cabb74578b32c0bc4
Kylee Tilley <kyleetilley@gmail.com>
2019-04-23 22:23:33 -0500

    Fixing run function

--- src/run.ts
+++ src/run.ts
@@ -1,8 +1,3 @@
 import callWith from "./utils/callWith";
-import Middlewear from "./types/Middlewear";
 
-type T = (...middlewears: Array<Middlewear>) => () => void;
-
-const run = (...tfs: Array<T>) => tfs.forEach(callWith());
-
-export default run;
+export default (...tfs: Array<() => void>) => tfs.forEach(callWith());

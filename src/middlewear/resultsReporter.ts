7aa4f2bcb82f69dcf2e7842e87f35dc73edebd1d
Kylee Tilley <kyleetilley@gmail.com>
2019-08-18 04:00:51 -0500

    Improve results reporter

--- src/middlewear/resultsReporter.ts
+++ src/middlewear/resultsReporter.ts
@@ -1,13 +1,47 @@
 import Setup from "../types/Setup";
 import { EventEmitter } from "events";
 import Results from "../types/Results";
+import groupBy from "../utils/groupBy";
+import Result from "../types/Result";
 
 export default function printResults(setup: Setup, events?: EventEmitter) {
   events.on("results", (results: Results) => {
     console.log("Results:\n");
-    results.forEach(result => {
-      console.log(JSON.stringify(result, jsonFriendlyErrorReplacer, 2));
-    });
+
+    Object.entries(groupBy<Result[]>(results, "testFilePath")).forEach(
+      entry => {
+        const [testFilePath, results] = entry;
+
+        console.log(
+          `[${testFilePath} (${results[results.length - 1].end.getTime() -
+            results[0].start.getTime()} ms)]:`
+        );
+
+        console.log();
+
+        results.forEach(result => {
+          console.log(
+            `- ${result.state}: ${result.description} (${result.end.getTime() -
+              result.start.getTime()} ms)`
+          );
+
+          if (result.state === "failed") {
+            console.log();
+            console.log(`${result.error.message}`);
+          }
+
+          if (result.state === "errored") {
+            console.log();
+            console.log(`Error: ${result.error.message}\n`);
+            console.log(`Stack: ${result.error.stack}`);
+          }
+
+          console.log();
+        });
+
+        console.log("\n");
+      }
+    );
   });
 }
 
---
+++ src/utils/groupBy.ts
@@ -0,0 +1,6 @@
+export default function<T>(xs, key): { [key: string]: T } {
+  return xs.reduce(function(rv, x) {
+    (rv[x[key]] = rv[x[key]] || []).push(x);
+    return rv;
+  }, {});
+}

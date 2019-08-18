44b63c6d505cfd2c8c943821ba8d4de35d756e41
Kylee Tilley <kyleetilley@gmail.com>
2019-08-18 02:07:49 -0500

    Refactor

--- src/middlewear/testUtils/exampleTests/spec/beforeEachTest.js
+++ src/middlewear/testUtils/exampleTests/spec/beforeEachTest.js
@@ -1,23 +1,23 @@
-describe("beforeEach", () => {
+describe("describe 1", () => {
   beforeEach(beforeEachMock);
 
-  it("should initialize value", testMock);
+  it("test 1", testMock);
 
-  describe("increment", () => {
+  describe("describe 2", () => {
     beforeEach(beforeEachMock);
 
-    it("should increment value", testMock);
+    it("test 2", testMock);
   });
 
-  describe("decrement", () => {
+  describe("describe 3", () => {
     beforeEach(beforeEachMock);
 
-    it("should decrement value", testMock);
+    it("test 3", testMock);
 
-    describe("again", () => {
+    describe("describe 4", () => {
       beforeEach(beforeEachMock);
 
-      it("should decrement value further", testMock);
+      it("test 4", testMock);
     });
   });
 });

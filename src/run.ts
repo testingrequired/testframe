3ed4a09c9580aeb88ebb577becc992885089bbf8
Kylee Tilley <kyleetilley@gmail.com>
2019-08-25 14:33:29 -0500

    Exit with 0 if no errors or failures

--- src/run.ts
+++ src/run.ts
@@ -1,3 +1,6 @@
 import callWith from "./utils/callWith";
 
-export default (...tfs: Array<() => void>) => tfs.forEach(callWith());
+export default (...tfs: Array<() => void>) => {
+  tfs.forEach(callWith());
+  process.exit(0);
+};

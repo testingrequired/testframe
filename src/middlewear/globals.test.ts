fba338cd2bd4bb1f122f1015d90d03ec73d7cf71
Kylee Tilley <kyleetilley@gmail.com>
2019-08-25 01:40:33 -0500

    Prevent duplicate global registration

--- src/middlewear/globals.test.ts
+++ src/middlewear/globals.test.ts
@@ -2,11 +2,25 @@ import globals from "./globals";
 import Setup from "../types/Setup";
 import createSetup from "./testUtils/createSetup";
 
-describe("component", () => {
-  const setup: Setup = createSetup();
+describe("globals", () => {
+  let setup: Setup;
+
+  beforeEach(() => {
+    setup = createSetup();
+  });
 
   it("should call fn with setup and results", () => {
     globals("foo", "bar")(setup);
     expect(setup.globals.foo).toEqual("bar");
+  });
+
+  it("should throw error if global already registered", () => {
+    globals("foo", "bar")(setup);
+
+    expect(() => {
+      globals("foo", "bar")(setup);
+    }).toThrow(
+      "Global 'foo' already registered. Please check your middlewear load order."
+    );
   });
 });
--- src/middlewear/globals.ts
+++ src/middlewear/globals.ts
@@ -1,5 +1,11 @@
 import Setup from "../types/Setup";
 
 export default (key, value) => (setup: Setup) => {
+  if (setup.globals[key]) {
+    throw new Error(
+      `Global '${key}' already registered. Please check your middlewear load order.`
+    );
+  }
+
   setup.globals[key] = value;
 };

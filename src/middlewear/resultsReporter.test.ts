16b0738a0111ea18c66e0e7fe2d572fbddf44d47
Kylee Tilley <kyleetilley@gmail.com>
2019-08-25 02:39:10 -0500

    Add test for resultsReporter

---
+++ src/middlewear/resultsReporter.test.ts
@@ -0,0 +1,101 @@
+import Setup from "../types/Setup";
+import Results from "../types/Results";
+import createSetup from "./testUtils/createSetup";
+import { resultsReporter } from ".";
+import { EventEmitter } from "events";
+import Result from "../types/Result";
+import createResult from "./testUtils/createResult";
+
+describe("resultsReporter", () => {
+  let setup: Setup;
+  let events: EventEmitter;
+  let results: Results;
+  let result: Result;
+  let consoleMock;
+
+  beforeEach(() => {
+    setup = createSetup();
+    events = new EventEmitter();
+    result = createResult("result1");
+    results = [result];
+    consoleMock = jest.spyOn(console, "log");
+  });
+
+  afterEach(() => {
+    consoleMock.mockReset();
+  });
+
+  describe("with passing result", () => {
+    beforeEach(() => {
+      result.state = "passed";
+    });
+
+    it("should work", () => {
+      resultsReporter(setup, events);
+      events.emit("results", results);
+      expect(consoleMock.mock.calls.length).toBe(6);
+      expect(consoleMock.mock.calls[0]).toEqual(["Results:\n"]);
+      expect(consoleMock.mock.calls[1]).toEqual([
+        "[test/file/path/result1.test.js (5 ms)]:"
+      ]);
+      expect(consoleMock.mock.calls[2]).toEqual([]);
+      expect(consoleMock.mock.calls[3]).toEqual([
+        "- passed: result1 description (5 ms)"
+      ]);
+      expect(consoleMock.mock.calls[4]).toEqual([]);
+      expect(consoleMock.mock.calls[5]).toEqual(["\n"]);
+    });
+  });
+
+  describe("with failing result", () => {
+    beforeEach(() => {
+      result.state = "failed";
+      result.error = new Error("error message");
+    });
+
+    it("should work", () => {
+      resultsReporter(setup, events);
+      events.emit("results", results);
+      expect(consoleMock.mock.calls.length).toBe(8);
+      expect(consoleMock.mock.calls[0]).toEqual(["Results:\n"]);
+      expect(consoleMock.mock.calls[1]).toEqual([
+        "[test/file/path/result1.test.js (5 ms)]:"
+      ]);
+      expect(consoleMock.mock.calls[2]).toEqual([]);
+      expect(consoleMock.mock.calls[3]).toEqual([
+        "- failed: result1 description (5 ms)"
+      ]);
+      expect(consoleMock.mock.calls[4]).toEqual([]);
+      expect(consoleMock.mock.calls[5]).toEqual(["error message"]);
+      expect(consoleMock.mock.calls[6]).toEqual([]);
+      expect(consoleMock.mock.calls[7]).toEqual(["\n"]);
+    });
+  });
+
+  describe("with errored result", () => {
+    beforeEach(() => {
+      result.state = "errored";
+      result.error = new Error("error message");
+      result.error.stack = "error stacktrace";
+    });
+
+    it("should work", () => {
+      resultsReporter(setup, events);
+      events.emit("results", results);
+      expect(consoleMock.mock.calls.length).toBe(9);
+      expect(consoleMock.mock.calls[0]).toEqual(["Results:\n"]);
+      expect(consoleMock.mock.calls[1]).toEqual([
+        "[test/file/path/result1.test.js (5 ms)]:"
+      ]);
+      expect(consoleMock.mock.calls[2]).toEqual([]);
+      expect(consoleMock.mock.calls[3]).toEqual([
+        "- errored: result1 description (5 ms)"
+      ]);
+      expect(consoleMock.mock.calls[4]).toEqual([]);
+      expect(consoleMock.mock.calls[5]).toEqual(["Error: error message\n"]);
+      expect(consoleMock.mock.calls[6]).toEqual(["Stack: error stacktrace"]);
+      expect(consoleMock.mock.calls[7]).toEqual([]);
+      expect(consoleMock.mock.calls[8]).toEqual(["\n"]);
+    });
+  });
+});
--- src/middlewear/testUtils/createResult.ts
+++ src/middlewear/testUtils/createResult.ts
@@ -1,12 +1,21 @@
 import Result, { ResultStates } from "../../types/Result";
 
-export default (id: string, state: ResultStates = "passed"): Result => ({
-  testFilePath: `test/file/path/${id}.test.js`,
-  description: `${id} description`,
-  state,
-  start: new Date(),
-  end: new Date(),
-  get time() {
-    return this.end.getTime() - this.start.getTime();
-  }
-});
+export default (
+  id: string,
+  state: ResultStates = "passed",
+  time: number = 5
+): Result => {
+  const start = new Date();
+  const end = new Date(start.getTime() + time);
+
+  return {
+    testFilePath: `test/file/path/${id}.test.js`,
+    description: `${id} description`,
+    state,
+    start,
+    end,
+    get time() {
+      return this.end.getTime() - this.start.getTime();
+    }
+  };
+};

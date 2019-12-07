21ba997b6d7c655c5228b6a623a3b57243ceabcf
Kylee Tilley <kyleetilley@gmail.com>
2019-12-07 15:41:21 -0600

    Refactor

--- src/middleware/matchTestFiles.ts
+++ src/middleware/matchTestFiles.ts
@@ -6,14 +6,15 @@ import Setup from "../types/Setup";
 export default (...patterns: Array<string>) => (setup: Setup) => {
   setup.testFilePaths = patterns
     .reduce(
-      (paths: Array<string>, pattern: string) => [
-        ...paths,
-        ...glob.sync(pattern, {
+      (paths: Array<string>, pattern) => {
+        const matchingFilePaths = glob.sync(pattern, {
           cwd: process.cwd(),
           absolute: true,
           ignore: ["./node_modules"]
-        })
-      ],
+        });
+
+        return [...paths, ...matchingFilePaths];
+      },
       []
     )
     .map(testFilePath =>

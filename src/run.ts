583716b178224acdbbe53de1bee545a17e7d47be
Kylee Tilley <kyleetilley@gmail.com>
2019-12-08 12:32:02 -0600

    Add documentation to run function

--- src/run.ts
+++ src/run.ts
@@ -1,4 +1,11 @@
-export default (...configs: Array<() => void>) => {
+/**
+ * Run a set of test configurations and exit
+ *
+ * @param configs Executable configs to run
+ */
+const run = (...configs: Array<() => void>) => {
   configs.forEach(config => config());
   process.exit(0);
 };
+
+export default run;

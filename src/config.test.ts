cd9505359ce3a2a885fe061e81759314603be2b3
Kylee Tilley <kyleetilley@gmail.com>
2019-12-08 12:03:13 -0600

    Add tests for config function

---
+++ src/config.test.ts
@@ -0,0 +1,45 @@
+import Middleware, { SetupExecutor, ResultsExecutor } from "./types/Middleware";
+import Setup from "./types/Setup";
+import config from "./config";
+
+describe("config", () => {
+  let middlewares: Array<Middleware>;
+  let middlewareASetupExector: SetupExecutor;
+  let middlewareAResultsExector: ResultsExecutor;
+  let middlewareBSetupExector: SetupExecutor;
+  let middlewareBResultsExector: ResultsExecutor;
+
+  beforeEach(() => {
+    middlewareAResultsExector = jest.fn();
+    middlewareASetupExector = jest.fn(
+      (setup: Setup) => middlewareAResultsExector
+    );
+
+    middlewareBResultsExector = jest.fn();
+    middlewareBSetupExector = jest.fn(
+      (setup: Setup) => middlewareBResultsExector
+    );
+
+    middlewares = [middlewareASetupExector, middlewareBSetupExector];
+  });
+
+  it("should not call middleware", () => {
+    config(...middlewares);
+
+    expect(middlewareASetupExector).not.toHaveBeenCalled();
+    expect(middlewareAResultsExector).not.toHaveBeenCalled();
+    expect(middlewareBSetupExector).not.toHaveBeenCalled();
+    expect(middlewareBResultsExector).not.toHaveBeenCalled();
+  });
+
+  describe("returned function", () => {
+    it("should call middleware", () => {
+      config(...middlewares)();
+
+      expect(middlewareASetupExector).toHaveBeenCalled();
+      expect(middlewareAResultsExector).toHaveBeenCalled();
+      expect(middlewareBSetupExector).toHaveBeenCalled();
+      expect(middlewareBResultsExector).toHaveBeenCalled();
+    });
+  });
+});

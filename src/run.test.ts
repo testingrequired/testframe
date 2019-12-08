134dfa2c3b10d1e7f59968640ce5d3a0bbb19dce
Kylee Tilley <kyleetilley@gmail.com>
2019-12-08 11:47:05 -0600

    Add tests for run

---
+++ src/run.test.ts
@@ -0,0 +1,33 @@
+import run from "./run";
+
+describe("run", () => {
+  let oldExit: any;
+  let configs: Array<() => void>;
+  let configA: jest.Mock;
+  let configB: jest.Mock;
+
+  beforeEach(() => {
+    oldExit = process.exit;
+    (process.exit as any) = jest.fn();
+
+    configA = jest.fn();
+    configB = jest.fn();
+
+    configs = [configA, configB];
+  });
+
+  afterEach(() => {
+    process.exit = oldExit;
+  });
+
+  it("should call each function in array", () => {
+    run(...configs);
+    expect(configA).toHaveBeenNthCalledWith(1);
+    expect(configB).toHaveBeenNthCalledWith(1);
+  });
+
+  it("should exit with 0", () => {
+    run(...configs);
+    expect(process.exit).toHaveBeenCalledWith(0);
+  });
+});

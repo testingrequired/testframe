194c6e281bac7ee2bb6fa1ab0401fc01e5f2cc8c
Kylee Tilley <kyleetilley@gmail.com>
2019-12-19 22:12:50 -0600

    Add test for passing seed to random

--- src/middleware/random.test.ts
+++ src/middleware/random.test.ts
@@ -16,4 +16,22 @@ describe("random", () => {
 
     expect(setup.globals.random).toBeInstanceOf(Chance);
   });
+
+  describe("with seed", () => {
+    const expectedSeed = "testSeed";
+
+    beforeEach(() => {
+      setup.args.seed = expectedSeed;
+    });
+
+    it("should pass seed", () => {
+      randomMiddleware(setup);
+
+      expect(setup.globals.random).toBeInstanceOf(Chance);
+
+      // This is the seed random assigns the expectedSeed
+      // There isn't a way to get the original passed seed
+      expect(setup.globals.random.seed).toBe(13936763416);
+    });
+  });
 });

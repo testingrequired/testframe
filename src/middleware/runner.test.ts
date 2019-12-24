aeed52f29aab0e2d288b2ab6706e8547ed473685
Kylee Tilley <kyleetilley@gmail.com>
2019-12-24 01:46:11 -0600

    Refactoring runner tests

--- src/middleware/runner.test.ts
+++ src/middleware/runner.test.ts
@@ -23,90 +23,119 @@ describe("runner", () => {
     setup.assertionErrorsTypes.push(AssertionError);
   });
 
-  it("should run test", () => {
-    runner(setup)(results);
-    expect(expectedTest.fn).toBeCalledWith();
-  });
-
-  it("should set test file path from", async () => {
-    await runner(setup)(results);
-    expect(results[0].testFilePath).toEqual(expectedTest.testFilePath);
-  });
-
-  it("should set description from test", async () => {
-    await runner(setup)(results);
-    expect(results[0].description).toEqual(expectedTest.description);
-  });
-
-  it("should set state to passed", async () => {
-    await runner(setup)(results);
-    expect(results[0].state).toEqual("passed");
-  });
-
-  it.todo("should set start date");
-
-  it.todo("should set end date");
-
-  describe("events", () => {
-    it("should emit on test start for each test", () => {
+  describe("when run state is run", () => {
+    it("should run test", () => {
       runner(setup)(results);
-      expect(setup.events.emit).toBeCalledWith("test:start", expectedTest);
+      expect(expectedTest.fn).toBeCalledWith();
     });
 
-    it("should emit on test result for each test", async () => {
-      expectedTests.push(createTest("2"));
+    it("should set test file path from", async () => {
       await runner(setup)(results);
-      expect(setup.events.emit).toBeCalledWith("test:result", results[0]);
-      expect(setup.events.emit).toBeCalledWith("test:result", results[1]);
+      expect(results[0].testFilePath).toEqual(expectedTest.testFilePath);
     });
 
-    describe("when test failure", () => {
+    it("should set description from test", async () => {
+      await runner(setup)(results);
+      expect(results[0].description).toEqual(expectedTest.description);
+    });
+
+    it("should set state to passed", async () => {
+      await runner(setup)(results);
+      expect(results[0].state).toEqual("passed");
+    });
+
+    it.todo("should set start date");
+
+    it.todo("should set end date");
+
+    describe("events", () => {
+      it("should emit on test start for each test", () => {
+        runner(setup)(results);
+        expect(setup.events.emit).toBeCalledWith("test:start", expectedTest);
+      });
+
+      it("should emit on test result for each test", async () => {
+        expectedTests.push(createTest("2"));
+        await runner(setup)(results);
+        expect(setup.events.emit).toBeCalledWith("test:result", results[0]);
+        expect(setup.events.emit).toBeCalledWith("test:result", results[1]);
+      });
+
+      describe("when test failure", () => {
+        let error: Error;
+
+        beforeEach(() => {
+          error = new AssertionError({});
+
+          (expectedTest.fn as any).mockImplementation(() => {
+            throw error;
+          });
+        });
+
+        it("should set state to failed", async () => {
+          await runner(setup)(results);
+          expect(results[0].state).toEqual("failed");
+        });
+
+        it("should emit test:failure", async () => {
+          await runner(setup)(results);
+          expect(setup.events.emit).toBeCalledWith("test:failure", results[0]);
+        });
+      });
+    });
+
+    describe("when test throws error", () => {
       let error: Error;
 
       beforeEach(() => {
-        error = new AssertionError({});
+        error = new Error();
 
         (expectedTest.fn as any).mockImplementation(() => {
           throw error;
         });
       });
 
-      it("should set state to failed", async () => {
+      it("should set state to errored", async () => {
         await runner(setup)(results);
-        expect(results[0].state).toEqual("failed");
+        expect(results[0].state).toEqual("errored");
       });
 
-      it("should emit test:failure", async () => {
+      it("should emit test:error", async () => {
         await runner(setup)(results);
-        expect(setup.events.emit).toBeCalledWith("test:failure", results[0]);
+        expect(setup.events.emit).toBeCalledWith("test:error", results[0]);
       });
-    });
-  });
 
-  describe("when test throws error", () => {
-    let error: Error;
-
-    beforeEach(() => {
-      error = new Error();
-
-      (expectedTest.fn as any).mockImplementation(() => {
-        throw error;
+      it("should set error to error thrown", async () => {
+        await runner(setup)(results);
+        expect(results[0].error).toEqual(error);
       });
     });
 
-    it("should set state to failed", async () => {
-      await runner(setup)(results);
-      expect(results[0].state).toEqual("errored");
-    });
+    describe("multiple tests", () => {
+      const expectedTest2: Test = {
+        testFilePath: "test file path 2",
+        description: "description 2",
+        runState: "run",
+        fn: jest.fn()
+      };
 
-    it("should emit test:error", async () => {
-      await runner(setup)(results);
-      expect(setup.events.emit).toBeCalledWith("test:error", results[0]);
-    });
+      const expectedTest3: Test = {
+        testFilePath: "test file path 3",
+        description: "description 3",
+        runState: "run",
+        fn: jest.fn()
+      };
 
-    it("should set error to error thrown", async () => {
-      await runner(setup)(results);
-      expect(results[0].error).toEqual(error);
+      beforeEach(() => {
+        expectedTests.push(expectedTest2, expectedTest3);
+      });
+
+      it("should run all tests", async () => {
+        await runner(setup)(results);
+        expect(expectedTest.fn).toBeCalledWith();
+        expect(expectedTest2.fn).toBeCalledWith();
+        expect(expectedTest3.fn).toBeCalledWith();
+      });
     });
   });
 
@@ -151,33 +180,6 @@ describe("runner", () => {
       expect(runner(setup)(results)).rejects.toEqual(
         "Invalid test run state: unknown"
       );
-    });
-  });
-
-  describe("multiple tests", () => {
-    const expectedTest2: Test = {
-      testFilePath: "test file path 2",
-      description: "description 2",
-      runState: "run",
-      fn: jest.fn()
-    };
-
-    const expectedTest3: Test = {
-      testFilePath: "test file path 3",
-      description: "description 3",
-      runState: "run",
-      fn: jest.fn()
-    };
-
-    beforeEach(() => {
-      expectedTests.push(expectedTest2, expectedTest3);
-    });
-
-    it("should run all tests", async () => {
-      await runner(setup)(results);
-      expect(expectedTest.fn).toBeCalledWith();
-      expect(expectedTest2.fn).toBeCalledWith();
-      expect(expectedTest3.fn).toBeCalledWith();
     });
   });
 });

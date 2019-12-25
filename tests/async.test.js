a6a979ce9344a0ddf9f682eae797cb5024ec1b47
Kylee Tilley <kyleetilley@gmail.com>
2019-12-25 16:07:22 -0600

    Update async testBetter tests that async await is really happening

--- tests/async.test.js
+++ tests/async.test.js
@@ -1,10 +1,27 @@
-describe("async test", () => {
-  it("should work", async () => {
-    await sleep(1000);
-    assert(true);
+describe("async", () => {
+  const expectedValue = Symbol("expected value");
+  let beforeEachValue;
+  let testValue;
+
+  beforeEach(async () => {
+    beforeEachValue = await asyncGetValue(expectedValue);
+  });
+
+  it("should work for before each", async () => {
+    assert(Object.is(beforeEachValue, expectedValue));
+  });
+
+  it("should work for test", async () => {
+    testValue = await asyncGetValue(expectedValue);
+    assert(Object.is(testValue, expectedValue));
   });
 });
 
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function asyncGetValue(input, delay = 1) {
+  await sleep(delay);
+  return input;
 }
